021e6c2a4113cdfa15611af131307725fb9f1427
Eusoj21 <clarens2015cedral@gmail.com>
2025-09-30 07:39:59 -0600

    Proyecto base parcial2

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <h1>Bienvenido a mi Parcial 2 🚀</h1>
+    <p>Este es un ejemplo sencillo en lugar del Hello World.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 40px;
+}
+</style>
